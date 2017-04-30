d32f970645a4ed22cadb200529a97fff9d73dfff
Atma Mani <amani@esri.com>
2017-04-30 11:10:45 -0700

    initial commit - sections 1 through 3

---
+++ byteShortInt/src/com/amani/Main.java
@@ -0,0 +1,25 @@
+package com.amani;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //byte variable
+        byte bVal = 56;
+
+        //short variable
+        short sVal = 900;
+
+        //int variable
+        int iVal = 8465;
+
+        //long variable
+        long lResult = 50_000 + 10*(bVal + sVal + iVal);
+
+        //float
+        double fVal = 4.8;
+
+        //print
+        System.out.println(lResult);
+    }
+}
---
+++ helloWorld/src/helloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by atma6951 on 4/30/17.
+ */
+public class helloWorld {
+    public static void main (String[] args)
+    {
+        System.out.println("Yo Atma");
+        int todayDate = 4302017;
+        int tomorrow = 512017;
+        System.out.println(String.format("Dates: {todayDate} {tomorrow}"));
+    }
+}
